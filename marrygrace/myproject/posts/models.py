bc5c35b57ad2959478f28706acb02822244ce5a5
marrygrace162 <capesosmarrygrace162@gmail.com>
2025-03-26 00:28:48 -0300

    Add files via upload

--- marrygrace/myproject/posts/models.py
+++ marrygrace/myproject/posts/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField()
-
+    body = models.TextField()
+    slug = models.SlugField()
+    date =models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.title
+   
--- marrygrace/myproject/posts/views.py
+++ marrygrace/myproject/posts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'posts/post_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
